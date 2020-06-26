3dddf1b9f384ea7d4dac61bdbe991c685ae050e0
Favodar <favodar@gmail.com>
2020-06-26 10:34:38 +0200

    deleted everything except src

--- catkin_ws/build/franka_gazebo-master/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryuga/catkin_ws/src/franka_gazebo-master
-# Build directory: /home/ryuga/catkin_ws/build/franka_gazebo-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/position_controllers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryuga/catkin_ws/src/position_controllers
-# Build directory: /home/ryuga/catkin_ws/build/position_controllers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
