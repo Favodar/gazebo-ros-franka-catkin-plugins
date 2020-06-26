83f82635152722d72e111669a980a017a46b4c36
Favodar <favodar@gmail.com>
2020-06-26 10:37:57 +0200

    readded devel

---
+++ catkin_ws/devel/share/gennodejs/ros/gazebo_msgs/msg/_index.js
@@ -0,0 +1,24 @@
+
+"use strict";
+
+let ODEJointProperties = require('./ODEJointProperties.js');
+let ContactState = require('./ContactState.js');
+let WorldState = require('./WorldState.js');
+let ODEPhysics = require('./ODEPhysics.js');
+let LinkStates = require('./LinkStates.js');
+let ContactsState = require('./ContactsState.js');
+let LinkState = require('./LinkState.js');
+let ModelState = require('./ModelState.js');
+let ModelStates = require('./ModelStates.js');
+
+module.exports = {
+  ODEJointProperties: ODEJointProperties,
+  ContactState: ContactState,
+  WorldState: WorldState,
+  ODEPhysics: ODEPhysics,
+  LinkStates: LinkStates,
+  ContactsState: ContactsState,
+  LinkState: LinkState,
+  ModelState: ModelState,
+  ModelStates: ModelStates,
+};
